a154917b87e4dacaa73a3c1725cb5284301d151f
Pol <pol.valverde@enti.cat>
2022-12-24 17:16:45 +0100

    lo hemos intentao :( (anuncis)

--- app/src/main/java/com/example/hangedroyale2ntry/gameScenes/GameActivity.kt
+++ app/src/main/java/com/example/hangedroyale2ntry/gameScenes/GameActivity.kt
@@ -5,6 +5,7 @@ import android.content.Context
 import android.content.Intent
 import android.content.IntentFilter
 import android.graphics.Color
+import android.media.MediaPlayer
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.core.view.isVisible
@@ -19,6 +20,13 @@ import retrofit2.Response
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import kotlin.math.roundToInt
+import com.google.android.gms.ads.rewarded.RewardedAd
+import android.view.View
+import com.google.android.gms.ads.*
+import com.google.android.gms.ads.rewarded.RewardItem
+import com.google.android.gms.ads.rewarded.RewardedAdLoadCallback
+import android.widget.Toast
+import com.google.android.gms.ads.rewarded.ServerSideVerificationOptions
 
 class GameActivity : AppCompatActivity() {
 
@@ -36,19 +44,70 @@ class GameActivity : AppCompatActivity() {
     private var timerStarted = false
     private lateinit var serviceIntent: Intent
     private var time = 0.0
+    private var mRewardedAd: RewardedAd? = null
+    private final var TAG = "MainMenuActivity"
+    var mMediaPlayer: MediaPlayer? = null
+
 
     override fun onCreate(savedInstanceState: Bundle?)
     {
+
         super.onCreate(savedInstanceState)
 
         binding = ActivityGameBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        var adRequest = AdRequest.Builder().build()
+        RewardedAd.load(this,"ca-app-pub-3940256099942544~3347511713", adRequest, object : RewardedAdLoadCallback(){
+            override fun onAdLoaded(ad: RewardedAd) {
+                //Log.d(TAG, "Ad was loaded.")
+                rewardedInterstitialAd = ad
+                val options = ServerSideVerificationOptions.Builder()
+                    .setCustomData("SAMPLE_CUSTOM_DATA_STRING")
+                    .build()
+                mRewardedAd?.setServerSideVerificationOptions(options)
+            }
+
+            override fun onAdFailedToLoad(adError: LoadAdError) {
+                //Log.d(TAG, adError?.toString())
+                mRewardedAd = null
+            }
+        })
+
+        mRewardedAd?.fullScreenContentCallback = object: FullScreenContentCallback() {
+            override fun onAdClicked() {
+                // Called when a click is recorded for an ad.
+                //Log.d(TAG, "Ad was clicked.")
+            }
+
+            override fun onAdDismissedFullScreenContent() {
+                // Called when ad is dismissed.
+                // Set the ad reference to null so you don't show the ad a second time.
+                //Log.d(TAG, "Ad dismissed fullscreen content.")
+                mRewardedAd = null
+            }
+
+            override fun onAdFailedToShowFullScreenContent(p0: AdError) {
+                // Called when ad fails to show.
+                //Log.e(TAG, "Ad failed to show fullscreen content.")
+                mRewardedAd = null
+            }
+
+            override fun onAdImpression() {
+                // Called when an impression is recorded for an ad.
+                //Log.d(TAG, "Ad recorded an impression.")
+            }
+
+            override fun onAdShowedFullScreenContent() {
+                // Called when ad is shown.
+                //Log.d(TAG, "Ad showed fullscreen content.")
+            }
+        }
+
         //chrono logic
         serviceIntent = Intent(applicationContext, TimerService::class.java)
         registerReceiver(updateTime, IntentFilter(TimerService.TIMER_UPDATED))
         startStopTimer()
-
         //buttons logic
         binding.pauseButton.setOnClickListener { startStopTimer() }
 
@@ -216,6 +275,22 @@ class GameActivity : AppCompatActivity() {
             checkLetterButton("M")
             binding.mButton.isEnabled = false
         }
+        binding.AdButton.setOnClickListener {
+
+            if(mRewardedAd!=null)
+            {
+                mRewardedAd?.show(this, OnUserEarnedRewardListener() {
+                    fun onUserEarnedReward(rewardItem: RewardItem) {
+                        var rewardAmount = rewardItem.amount
+                        var rewardType = rewardItem.type
+                        Toast.makeText(this, ("Can load Ad"), Toast.LENGTH_SHORT).show()
+                    }
+                })
+            } else {
+                Toast.makeText(this, ("Can't load Ad"), Toast.LENGTH_SHORT).show()
+            }
+
+        }
 
 
     }
@@ -265,6 +340,7 @@ class GameActivity : AppCompatActivity() {
         binding.youLoseLeaderboardButton.isVisible = true
         binding.youLoseReloadButton.isVisible = true
         binding.youLoseScoreText.isVisible = true
+        binding.AdButton.isVisible = true
     }
 
     fun checkLetterButton(letter:String)
@@ -369,4 +445,15 @@ class GameActivity : AppCompatActivity() {
         time = 0.0
         binding.ChronoID.text = getTimeStringFromDouble(time)
     }
+
+    //MUSIC ADDED
+    /*
+    fun playSound(loop:Boolean) {
+        if (mMediaPlayer == null) {
+            mMediaPlayer = MediaPlayer.create(this, R.raw.test)
+            mMediaPlayer!!.isLooping = loop
+            mMediaPlayer!!.start()
+        } else mMediaPlayer!!.start()
+    }
+    */
 }
--- app/src/main/java/com/example/hangedroyale2ntry/gameScenes/MainMenuActivity.kt
+++ app/src/main/java/com/example/hangedroyale2ntry/gameScenes/MainMenuActivity.kt
@@ -3,20 +3,31 @@ package com.example.hangedroyale2ntry.gameScenes
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.example.hangedroyale2ntry.databinding.ActivityMainMenuBinding
+import com.google.android.gms.ads.*
+import com.google.android.gms.ads.rewarded.RewardedAd
+import com.google.android.gms.ads.rewarded.RewardedAdLoadCallback
 
 class MainMenuActivity : AppCompatActivity()
 {
     private lateinit var binding: ActivityMainMenuBinding
 
+
     override fun onCreate(savedInstanceState: Bundle?)
     {
         super.onCreate(savedInstanceState)
 
+
+
+
         binding = ActivityMainMenuBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+
+
         binding.menuPlayButton.setOnClickListener{
+            // We preload the Ad
             val intent = Intent(this@MainMenuActivity, GameActivity::class.java)
             startActivity(intent)
             finish()
